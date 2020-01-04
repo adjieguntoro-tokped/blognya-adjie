6e74145653eaba084195bf89f2c94d27e51d73f0
Adjie Guntoro <adjie.guntoro@tokopedia.com>
2020-01-04 23:30:48 +0700

    first commit in 2020

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    title: `adjieguntoro.com`,
+    author: `Adjie Guntoro`,
+    description: `Ini deskripsi blog nya`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `adjieguntoro`,
     },
   },
   plugins: [
