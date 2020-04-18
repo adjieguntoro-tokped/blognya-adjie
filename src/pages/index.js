b5e190f76e61278cd8ee4b716541b5714df726be
Adjie Guntoro <adjie.guntoro@tokopedia.com>
2020-04-18 23:45:31 +0700

    change homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,8 @@ const styles = {
   },
   content: {
     display: "block",
-    fontSize: `${14 / 16}rem`,
+    fontSize: `${20 / 16}rem`,
+    lineHeight: "1.9rem",
     color: "#afafbf",
   },
 }
@@ -31,13 +32,17 @@ const Home = () => {
       <div style={styles.container}>
         <div style={styles.wrapper}>
           <code style={styles.content}>
-            Hi, There! <br />
+            Hi There! 👋
+            <br />
+            <br />
+            <br />
             Adjie Guntoro is here.
             <br />
-            Currently work as Sofware Engineer at Tech Company in Jakarta,
-            Indonesia.
             <br />
-            Web Development enthusiast, movies and also books!
+            I'm a 24 years old. Currently, I work as Sofware Engineer at Tech
+            Company in Jakarta.
+            <br />
+            Web development enthusiast, movies and also books!
             <br />
             em: adjie1607@gmail.com | tw:{" "}
             <a target="_blank" href="https://twitter.com/adjieguntoro">
